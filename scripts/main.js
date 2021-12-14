ec3fbb27a5525b76845a9d0a0be08e9c94cf79b5
Haruki Tamura <76424169+reboki@users.noreply.github.com>
2021-12-15 01:17:34 +0900

    changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     const hero = new HeroSlider('.swiper-container');
     hero.start();
-});
 
+
+    const header = document.querySelector('.header');
+    const _navAnimation = function (el, inview) {
+        if (inview) {
+            header.classList.remove('triggered');
+        } else {
+            header.classList.add('triggered');
+        }
+    }
+
+    new MobileMenu();
+
+});
